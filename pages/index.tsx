d5474e8d1ae2b4eea8a0e40c4276a04787cc73c1
SethSharp <sesharp@outlook.com>
2022-02-06 14:05:32 +1000

    Initial commit, set up Project with TailWindCSS and Sanity Schema

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <h1> Is Alex Gae? </h1>
+      <h2> Yo</h2>
+      <p>
+        I dont know
+      </p>
+    </div>
+    
+
+  )
+}
